2fe5629acaf8ec1be4b113af342dd8fcaf16f2c7
elias rizik <eliasrizik@gmail.com>
2025-07-16 12:00:17 +0300

    connect server and client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   const [message, setMessage] = useState('');
   useEffect(() => {
-    fetch('http://localhost:3001/api/hello')
+    fetch('http://localhost:3000/api/hello')
       .then((res) => res.text())
       .then(setMessage);
   }, []);
